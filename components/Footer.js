58eb49230fedb8eb28279f6f2ac4a761ef9c14c5
4techguns <4techgunsinbox@gmail.com>
2022-03-09 19:00:20 -0330

    PAKISTAN ZINDABAD (thanks ragarnoky)

--- components/Footer.js
+++ components/Footer.js
@@ -8,6 +8,7 @@ export default function Footer()
             <p>Copyright &copy; Bluen&apos;t 2022. All rights not reserved lmao you think we have lawyers for a personal site and a non-registered company i do not care what you do go steal the code on my <a href="https://github.com/4techguns/4techgunsdotga">github</a> for all i care</p>
             <a href="a.png">Built with KarbML 6&trade;</a>
             <p className={styles.comiksand}>POWERED BY KARBIS STUDIO BACKPAGE</p>
+            <p className={styles.comiksand}>ALSO POWERED BY PAKISTAN SERVER IN LAHORE WITH 66954MS PING</p>
         </div>
     )
 }
