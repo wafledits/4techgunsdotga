802f0930fba07f68bcc785f44860d33126f8066d
4techguns <4techgunsinbox@gmail.com>
2022-03-09 21:14:22 -0330

    Damn I always mix up these

--- components/Logo.js
+++ components/Logo.js
@@ -7,7 +7,7 @@ export default function Logo() {
     return (
         <div>
             <div className={styles.logoContainer}>
-                <Image src={moment().month() == 8 && moment().day() == 19 ? "/iconDark.png" : "/iconDarkBirthdayEasterEgg.png"} width="84" height="64" className={styles.logoMarginned} />
+                <Image src={moment().month() == 8 && moment().day() == 19 ? "/iconDarkBirthdayEasterEgg.png" : "/iconDark.png"} width="84" height="64" className={styles.logoMarginned} />
                 &nbsp;<p className={styles.logoText}>4techguns.ga</p>
             </div>
             <Quote />
