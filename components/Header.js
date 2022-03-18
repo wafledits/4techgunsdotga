cfdeab474c39e7dee242e82645be219f05d8f52c
wafledits <101883252+wafledits@users.noreply.github.com>
2022-03-18 11:08:34 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,11 +5,19 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <div>
-      <Link href="/" ><a className={styles.defaultLinkButton}>&#xf7db; Home</a></Link>
+      <Link href="/dash" ><a className={styles.defaultLinkButton}>&#xf7db; Home</a></Link>
       |
-      <Link href="/about"><a className={styles.defaultLinkButton}>&#xf7fb; About</a></Link>
+      <Link href="/games"><a className={styles.defaultLinkButton}>&#xf11b; Games</a></Link>
       |
-      <Link href="/pyseries"><a className={styles.defaultLinkButton}>The Py Series</a></Link>
+      <Link href="/avatar"><a className={styles.defaultLinkButton}>&#xf7fb; Avatar</a></Link>
+      |
+      <Link href="/create"><a className={styles.defaultLinkButton}>&#xf5e2; Create</a></Link>
+      |
+      <Link href="/users"><a className={styles.defaultLinkButton}>&#xf50d; Users</a></Link>
+      |
+      <Link href="https://discord.gg/npwhfdZFpW"><a className={styles.defaultLinkButton}>&#xfb6e; Discord</a></Link>
+      |
+      <Link href="/"><a className={styles.defaultLinkButton}>&#xfafc; Log Out</a></Link>
       <hr />
     </div>
   )
